151798594122058baa56bc0c6dbf91442a1796f3
Tobias Gwiß <tobias.gwiss@josephinum.at>
2024-04-08 14:28:13 +0200

    Add project files.

---
+++ Projekt_08_Bsteh_Gwiß/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Projekt_08_Bsteh_Gwiß
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

e01a9fb2f2fe02e8fc6cb52e6c845b0db08ce978
Lukas Bsteh <lukas.bsteh@josephinum.at>
2024-04-16 14:53:17 +0200

    connecten jedes knopfes vergrößern und verschieben des tisches

--- Projekt_08_Bsteh_Gwiß/MainWindow.xaml.cs
+++ Projekt_08_Bsteh_Gwiß/MainWindow.xaml.cs
@@ -21,6 +21,226 @@ namespace Projekt_08_Bsteh_Gwiß
             InitializeComponent();
         }
 
-     
+        
+
+        
+
+        private void Rot1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Schwarz2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Rot3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Schwarz4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+         private void Rot5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Schwarz6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rot7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Schwarz8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Schwarz10_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rot12_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Schwarz13_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rot14_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Schwarz15_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rot16_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Schwarz17_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rot18_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rot19_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Schwarz20_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rot21_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Schwarz22_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Rot23_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Schwarz24_Click(object sender, RoutedEventArgs e)
+                {
+
+                }
+        private void Rot25_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Schwarz26_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Rot27_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Schwarz28_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Schwarz29_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rot30_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Schwarz31_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Rot32_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Schwarz33_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rot34_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Schwarz35_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Rot36_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Weiss0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Weiss1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Weiss13_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Weiss25_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void even_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void half1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void red_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void black_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void half2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void odd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Linie1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Linie2_Click(object sender, RoutedEventArgs e)
+                {
+
+                }
+        private void Linie3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
     }
 }
